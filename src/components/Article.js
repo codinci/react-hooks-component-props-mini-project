640209367e4b94db95f27e8337d816ccf341be00
codinci <dkibau@gmail.com>
2022-09-22 10:19:52 +0300

    Completed assignment

---
+++ src/components/Article.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+function Article({ title, date = "January 1, 1970", preview, minutes }) {
+  function roundNearest5(num) {
+    return Math.round(num / 5);
+  }
+  function roundNearest10(num) {
+    return Math.round(num / 10);
+  }
+
+  function displayEmoji() {
+    const emojiArray = [];
+    if (minutes <= 30) {
+      for (let i = 0; i < roundNearest5(minutes); i++) {
+        emojiArray.push("☕️");
+      }
+     return(emojiArray.join(' ') + ' ' + minutes + " min read");
+    } else {
+      for (let i = 0; i < roundNearest10(minutes); i++) {
+        emojiArray.push("🍱");
+      }
+      return emojiArray.join('') + " " + minutes + " min read";
+    }
+  }
+
+  return (
+    <article>
+      <h3>{title}</h3>
+      <small>{date}</small>
+      <span>{displayEmoji() }</span>
+      <p>{preview}</p>
+    </article>
+  );
+}
+
+export default Article;
